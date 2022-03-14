5ed1e8e90bfede77afddf8dda20290bf17b37d1d
Roman831 <101548775+Roman831@users.noreply.github.com>
2022-03-14 13:47:17 +0530

    Add files via upload

---
+++ springboot/src/main/java/springboot/SpringApi.java
@@ -0,0 +1,18 @@
+package springboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SpringApi {
+
+
+
+public static void main(String[] args) {
+SpringApplication.run(SpringApi.class, args);
+}
+
+
+
+}
